9a5fc1d66de4ac51103397e8bdbe451d41136d20
Alexander <Joker124rus@gmail.com>
2022-03-14 22:58:59 +0700

    Add inherit from mono behaviour #P5

--- Assets/LearnMaterials 2/Scripts/AlexanderScript.cs
+++ Assets/LearnMaterials 2/Scripts/AlexanderScript.cs
@@ -9,9 +9,15 @@ namespace Assets.LearnMaterials_2.Scripts
 {
     public class AlexanderScript : FatherScript
     {
+        private Transform target;
+        public AlexanderScript(Transform target)
+        {
+            this.target = target;
+        }
         public override void Use()
         {
-
+            target.Rotate(new Vector3(30,30), 45);
+            // target.DetachChildren();
         }
     }
 }
--- Assets/LearnMaterials 2/Scripts/FatherScript.cs
+++ Assets/LearnMaterials 2/Scripts/FatherScript.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.LearnMaterials_2.Scripts
 {
-    public abstract class FatherScript
+    public abstract class FatherScript : MonoBehaviour
     {
         public abstract void Use();
     }
