dff848c84a6ba81f38d132cfefc5ddff129d7c71
Sofya <vampiresszero@gmail.com>
2022-03-22 14:00:47 +0700

    Task done #P5

--- Assets/LearnMaterials 2/Scripts/MovementScript.cs
+++ Assets/LearnMaterials 2/Scripts/MovementScript.cs
@@ -32,7 +32,7 @@ namespace Assets.LearnMaterials_2.Scripts
         {
             if (move == true)
             {
-                UseAsync();
+                Use();
             }
         }
 
---
+++ Assets/LearnMaterials 2/Scripts/SuperScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.LearnMaterials_2.Scripts
+{
+    public class SuperScript : FatherScript
+    {
+        public List<FatherScript> scripts;
+
+        [ContextMenu("USE EVERYTHING")]
+        public override void Use()
+        {
+            foreach(FatherScript script in scripts)
+            {
+                script.Use();
+            }
+        }
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
